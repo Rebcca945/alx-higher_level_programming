6c082d335f916a21b7ac96c38ffa7894c9ce1d13
Rebcca945 <106800841+Rebcca945@users.noreply.github.com>
2022-10-20 00:26:32 +0300

    Update 0-rectangle.py

--- 0x08-python-more_classes/0-rectangle.py
+++ 0x08-python-more_classes/0-rectangle.py
@@ -1,8 +1,9 @@
+#!/usr/bin/python3
 """
-Defines an empty class Rectangle
+Defining an empty class Rectangle
 """
 
 
 class Rectangle:
-    """Empty representation of a rectangle"""
+    """Empty representation of a Rectangle"""
     pass
